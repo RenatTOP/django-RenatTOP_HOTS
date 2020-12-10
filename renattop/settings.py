a3a83abf5de7e184b1ab4f11bb70f25a0dabb2a7
RenatTOP <renatseverski@gmail.com>
2020-12-10 14:27:16 +0200

    fix-send

--- renattop/settings.py
+++ renattop/settings.py
@@ -147,8 +147,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
